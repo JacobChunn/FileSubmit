f4aa66df943ebfd881a9ca27b9fe0d598d077703
JacobChunn <chunnjake@gmail.com>
2024-02-07 13:13:16 -0500

    modified:   app/ui/employees/edit/form-bool-entry.tsx

--- app/ui/employees/edit/form-bool-entry.tsx
+++ app/ui/employees/edit/form-bool-entry.tsx
@@ -1,4 +1,4 @@
-import { EmployeeState } from "@/app/lib/actions"
+import { EmployeeState } from "@/app/lib/definitions";
 import { ChangeEvent, SetStateAction, useEffect, useState } from "react";
 
 export type Props = {
