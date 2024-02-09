7646af82732e6530df763c6ab89a920840b7109d
Jake Chunn <chunnjake@gmail.com>
2024-02-08 22:34:11 -0500

    modified:   app/login/page.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,56 +7,40 @@ import Image from 'next/image';
 import Logo from '@/app/EncoreLogo.jpg'
 
 export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-24 items-start">
-        <div className='h-full relative'>
-          <Image
-              src={Logo}
-              alt={'Encore Logo'}
-              width={0}
-              height={0}
-              sizes="100vw"
-              className='object-cover'
-              style={{
-                borderRadius: '10px',
-                width: 'auto',
-                height: '100%'
-              }}
-            />
-          </div>
-        <div className=' w-96 rounded-lg bg-blue-500'/>
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Encore.</strong>
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshot of the dashboard project showing mobile version"
-          />
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col p-6">
+			<div className="flex h-24 items-start rounded-lg bg-blue-500"/>
+			<div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+				<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+					<p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+						<strong>Welcome to Encore.</strong>
+					</p>
+					<Link
+						href="/login"
+						className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+					>
+						<span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+					</Link>
+				</div>
+				<div className="flex items-center justify-center p-6 bg-gray-50">
+					{/* Add Hero Images Here */}
+					<div className='h-full relative p-12'>
+						<Image
+							src={Logo}
+							alt={'Encore Logo'}
+							width={0}
+							height={0}
+							sizes="100vw"
+							className='object-cover'
+							style={{
+								borderRadius: '10px',
+								width: 'auto',
+								height: '100%'
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
