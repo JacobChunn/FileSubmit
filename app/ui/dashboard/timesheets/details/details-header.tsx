036cd81640bb09ce52576d8a78ef61105595cbf2
JacobChunn <chunnjake@gmail.com>
2024-04-12 17:03:14 -0400

    added sign button

--- app/ui/dashboard/timesheets/details/details-header.tsx
+++ app/ui/dashboard/timesheets/details/details-header.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { addTimesheetDetails } from "@/app/lib/actions"
 import { Button } from "@/app/ui/button"
 import { useContext, useEffect, useState } from "react"
 import { TimesheetContext } from "../table/timesheet-wrapper"
@@ -31,44 +30,56 @@ export default function TimesheetDetailsHeader({
 		);
 	}
 
+	const signTimesheet = () => {
+		//TODO: FINISH
+	}
+
+	const addTSD = () => {
+		const currentTSDs = context.localTimesheetDetails || [];
+		const newTSD: TimesheetDetails = {
+			id: 0,
+			timesheetid: timesheetID,
+			employeeid: context.employeeid,
+			projectid: 0,
+			phase: 0,
+			costcode: 0,
+			description: null,
+			mon: 0,
+			monot: 0,
+			tues: 0,
+			tuesot: 0,
+			wed: 0,
+			wedot: 0,
+			thurs: 0,
+			thursot: 0,
+			fri: 0,
+			friot: 0,
+			sat: 0,
+			satot: 0,
+			sun: 0,
+			sunot: 0,
+			lasteditdate: ""
+		}
+		context.setLocalTimesheetDetails([...currentTSDs, newTSD]);
+	}
+
 	return (
 		<div className="w-full h-full">
 			<div className="flex justify-end py-4">
+				<button
+					className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+					onClick={signTimesheet}
+				>
+					Sign Timesheet
+				</button>
+				<div className="w-2"/>
 				<Button
-					onClick={() =>{
-						//addTimesheetDetails(timesheetID)
-						const currentTSDs = context.localTimesheetDetails || [];
-						const newTSD: TimesheetDetails = {
-							id: 0,
-							timesheetid: timesheetID,
-							employeeid: context.employeeid,
-							projectid: 0,
-							phase: 0,
-							costcode: 0,
-							description: null,
-							mon: 0,
-							monot: 0,
-							tues: 0,
-							tuesot: 0,
-							wed: 0,
-							wedot: 0,
-							thurs: 0,
-							thursot: 0,
-							fri: 0,
-							friot: 0,
-							sat: 0,
-							satot: 0,
-							sun: 0,
-							sunot: 0,
-							lasteditdate: ""
-						}
-						context.setLocalTimesheetDetails([...currentTSDs, newTSD]);
-					}}
+					onClick={addTSD}
 				>
 					Add Timesheet Details
-					{children}
 				</Button>
 			</div>
+			{children}
 		</div>
 	)
 
